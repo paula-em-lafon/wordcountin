52c55467382c5079d1671a16fd4f2ed7aca8ea0f
Pablo-r4001 <pablolafongarc@gmail.com>
2016-02-25 15:24:55 -0700

    settings right

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
 
+from flask import Flask
+import os
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
